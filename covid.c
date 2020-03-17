85d781d2464e12b001bc8df7d62f53dd7871c1c1
naryand <naryand@sfu.ca>
2020-03-17 01:18:32 -0700

    initial commit

---
+++ covid.c
@@ -0,0 +1,137 @@
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#define DAYS 365
+
+// C implementation of covid.py, used for regression on to data points to get parameters
+// Mostly because this is why faster than the python implementation
+
+typedef struct country {
+    char country[10];
+    float suscept;
+    float infect;
+    float immune;
+} country;
+
+void spread(float factor, struct country *world) {
+    if(world->suscept == 0 || world->infect == 0) {
+        return;
+    }
+    float infected = (factor-1)*world->infect*(1-((world->infect+world->immune)/world->suscept));
+    world->suscept -= infected;
+    if(world->suscept < 0) {
+        world->infect += world->suscept;
+        world->suscept = 0;
+        return;
+    }
+    world->infect += infected;
+}
+
+void recover(float factor, struct country *world, float days) {
+    if(world->suscept == 0) {
+        return;
+    }
+    float recovered = world->infect/(pow(factor, days));
+    world->infect -= recovered;
+    if(world->infect < 0) {
+        world->immune += world->infect;
+        world->infect = 0;
+        return;
+    }
+    world->immune += recovered;
+}
+
+// Implement a regression algorithm to get growth factor and
+// infectious period parameters from start and end data points.
+
+typedef struct result {
+    float factor;
+    float days;
+} result;
+
+result regression() {
+    struct country world;
+    strcpy(world.country, "world");
+    // regression initial conditions for start
+    int populn = 7800000; // world pop.
+    float factor = 1.166754; // init growth factor
+    float days = 18.085819; // init infectious period
+    int infect = 1; // init infections
+    int recovd = 0; // init recoveries
+    int length = 47; // length of each simulation in days
+
+    // regression targets
+    float infend = 53.3;
+    float immend = 73;
+
+    // margin between result and target
+    float margin = 20;
+    // step of parameters after each loop
+    float step = 0.001;
+
+    while(1) {
+        // init world struct
+        world.infect =  (float) infect;
+        world.immune = (float) recovd;
+        world.suscept = (float) populn-infect-recovd;
+
+        // complete a simulation over the length
+        for(int i = 0; i < length; i++) {
+            spread(factor, &world);
+            recover(factor, &world, days);
+        }
+
+        // difference between targets and results
+        float infdiffer = infend-world.infect;
+        float immdiffer = immend-world.immune;
+
+        // adjust parameters accordingly if outside margin
+        if(world.infect <= infend-margin || world.infect >= infend+margin) {
+            factor += step*infdiffer;
+        }
+        if(world.immune >= immend+margin || world.immune <= immune-margin) {
+            days += step*immdiffer;
+        }
+        // break if parameters all within margin
+        if(world.infect <= infend+margin && world.infect >= infend-margin && world.immune <= immend+margin && world.immune >= immend-margin) {
+            break;
+        }
+    }
+
+    // return regression results
+    result ret;
+    ret.factor = factor;
+    ret.days = days;
+    return ret;
+}
+
+
+int main() {
+    // do the regression
+    result ret;
+    ret = regression();
+    printf("factor %.6f days %.6f\n", ret.factor, ret.days);
+
+    // mostly same as in covid.py
+    int infect = 46;
+    int recovd = 68;
+    int populn = 7800000;
+    float factor = 1.15;
+    int days = DAYS;
+    int length = 14;
+
+    struct country world;
+    strcpy(world.country, "world");
+    world.suscept = (float) populn-infect-recovd;
+    world.infect =  (float) infect;
+    world.immune = (float) recovd;
+
+    int aggr[DAYS]; // DAYS defined up top
+    for(int i = 1; i < length+1; i++) {
+        spread(factor, &world);
+        recover(factor, &world, days);
+        aggr[i] = world.infect+world.immune;
+        //printf("Day: %d Susceptible: %.3f Infected: %.3f Immune: %.4f Cumulative: %.3f\n", i, world.suscept, world.infect, world.immune, world.infect+world.immune);
+    }
+    return 0;
+}
---
+++ covid.py
@@ -0,0 +1,74 @@
+# list of countries, country has object containing, name, susceptible pop., infected pop., immune pop.
+# simulate spread (travel btwn countries?)
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
+
+# 
+class country():
+    def __init__(self, country, suscept, infect, immune):
+        self.name = country
+        self.suscept = suscept
+        self.infect = infect
+        self.immune = immune
+
+# 87k current cases, 87k recoveries, 6k deaths worldwide
+# assume the dead are immune
+infect = 87
+recovd = 87
+populn = 7800000
+# per day multiplication of cases
+factor = 1.189060
+# infection period in days
+days = 14.451526
+# simulation length in days
+length = 10
+
+# initialize world
+world = country("world", populn-infect-recovd, infect, recovd)
+
+def spread(factor, country):
+    if world.suscept == 0 or world.infect == 0: # none left to infect or eradicated
+        return
+    
+    # number of new cases with logistic growth
+    infected = (factor-1)*world.infect*(1-((world.infect+world.immune)/world.suscept))
+    world.suscept -= infected # subtract new cases from suscept
+
+    if world.suscept < 0: # prevent suscept from going negative
+        world.infect += world.suscept
+        world.suscept = 0
+        return
+    
+    world.infect += infected # add new cases to infected
+
+def recover(factor, country, days):
+    if world.infect == 0: # if eradicated
+        return
+    
+    # number of recoveries
+    recovered = world.infect/(factor**days) # approximation of cases that were in progress days ago
+    world.infect -= recovered # subtract recovered from infected
+
+    if world.infect < 0: # prevent infect from going negative
+        world.immune += world.infect
+        world.infect = 0
+        return
+
+    world.immune += recovered # add recovered to immune
+
+# aggregate infections list
+aggrlist = [None] * (length)
+
+# simulate over the length
+for i in range(1, length+1):
+    spread(factor, world)
+    recover(factor, world, days)
+    aggrlist[i-1] = world.infect+world.immune # add cumulative to list
+    # print simulation results by day
+    #print("Day: {:d} Susceptible: {:.3f} Infected: {:.3f} Immune: {:.3f} Cumulative: {:.3f} ".format(i, world.suscept, world.infect, world.immune, world.infect+world.immune))
+
+# plot simulation results
+#plt.yticks(ticks=range(0,1000000,100000))
+plt.grid(b=True)
+plt.plot(range(1,length+1), aggrlist)
+plt.savefig("aggr.png")
